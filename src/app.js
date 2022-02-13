da3a5db86a05437d24fbdc7f2e1349ef56df63cd
Cris Crawford <69829777+cmcrawford2@users.noreply.github.com>
2022-02-12 20:29:00 -0500

    Rearrange app.get calls

--- src/app.js
+++ src/app.js
@@ -6,29 +6,11 @@ import indexRouter from './routes/index';
 
 const app = express();
 
-app.use(cors());
-
-// app.options('*', cors());
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 app.use('/v1', indexRouter);
-
-// // Add Access Control Allow Origin headers
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   next();
-// });
-
-// app.listen(80, () => {
-//   // eslint-disable-next-line no-console
-//   console.log('CORS-enabled web server listening on port 80');
-// });
 
 export default app;
